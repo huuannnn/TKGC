e4bf4bece855b5ddde33eab5a8088fd2f69949ef
huuannnn <nguyenhuuan.research@gmail.com>
2026-05-17 06:34:36 +0000

    Remove core directory

--- core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dataset import TKGDataset, TKGDataLoader
-from .trainer import Trainer
-from .oracle_trainer import OracleTrainer
-from .logger import Logger
-
-__all__ = ['TKGDataset', 'TKGDataLoader', 'Trainer', 'OracleTrainer', 'Logger']
